8d09a9a21be0e9e356ed5179ea6a96aafa5f3fee
bhagyashree <bhaggo30july@gmail.com>
2024-09-10 15:03:15 +0530

    banking management system

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer {
+
+    private String name;
+    private String Customerid;
+
+    public Customer(String name,String Customerid)
+    {
+        this.name=name;
+        this.Customerid=Customerid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerid() {
+        return Customerid;
+    }
+
+    public void setCustomerid(String customerid) {
+        Customerid = customerid;
+    }
 }
